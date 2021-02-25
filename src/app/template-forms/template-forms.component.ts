bedfc225db435e845d178799253c61aa00a74440
chandreshbaboo <chandresh.baboo@gmail.com>
2021-02-25 22:51:15 +0530

    One Way binding in the template driven form

--- src/app/template-forms/template-forms.component.ts
+++ src/app/template-forms/template-forms.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -8,17 +8,32 @@ import { NgForm } from '@angular/forms';
 })
 export class TemplateFormsComponent implements OnInit {
   @ViewChild('f') signupForm: NgForm;
+  public userModel = {
+    firstName: 'Chandresh',
+    lastName: 'baboo',
+    email: 'chandresh.baboo@gmail.com'
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
 
   public onSubmit(form: NgForm) {
+    // here new value will be available
     console.log('submit', form.value);
   }
 
   onClear() {
     // Now that we have access to the form via the 'ViewChild', we can access the form and clear it.
     this.signupForm.reset();
+  }
+
+  onChangeFirstName(event) {
+    console.log('new Value=', event);
+    /*
+      if we use only one way binding, then we can send the values from the model to view.
+      it we change the value in view, will not reflect in the model.
+    */
+    console.log('onChangeFirstName', this.userModel);
   }
 }
